507f40defef4a0dff02745e418eac1c6254b903a
giovannimazzi <gioelor@libero.it>
2026-03-13 18:18:52 +0100

    add SHOW - MILESTONE 4

--- controllers/dbpostController.js
+++ controllers/dbpostController.js
@@ -10,7 +10,17 @@ function index(req, res) {
   });
 }
 
-function show(req, res) {}
+function show(req, res) {
+  // recupero id da URL
+  const id = req.params.id;
+  const sql = "SELECT * FROM posts WHERE id = ?";
+  connection.query(sql, [id], (err, results) => {
+    if (err) return res.status(500).json({ error: "Database query failed" });
+    if (results.length === 0)
+      return res.status(404).json({ error: "Post not found" });
+    res.json(results[0]);
+  });
+}
 
 function store(req, res) {}
 
